bebe4eb68396187576558f9ce0a1281d73c8a121
Erdi Irden <erdirden3@gmail.com>
2023-06-16 00:36:59 +0300

    APK eklendi

---
+++ KossCarAPK/KossCar_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,141 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void kamera::LateUpdate()
+extern void kamera_LateUpdate_mEE66B361D213B0CA84BE55F362AB38F2EE08C39D (void);
+// 0x00000002 System.Void kamera::.ctor()
+extern void kamera__ctor_mFC815A26481319405330DDC655F061972E58F886 (void);
+// 0x00000003 System.Void klon_olustur::Start()
+extern void klon_olustur_Start_mA712B5520D4DF322D82A1FE21BDB1655CDD12FDE (void);
+// 0x00000004 System.Void klon_olustur::nesne_klonla()
+extern void klon_olustur_nesne_klonla_mE07B4676DC5D97F671D608776E64FC29EED092E9 (void);
+// 0x00000005 System.Void klon_olustur::klonla(UnityEngine.GameObject,System.Single)
+extern void klon_olustur_klonla_m6D375F32F51221BB769C63B01F1E0869AD921434 (void);
+// 0x00000006 System.Void klon_olustur::Update()
+extern void klon_olustur_Update_m50872A12CD8230377F9466675BC7E946AA0C4E57 (void);
+// 0x00000007 System.Void klon_olustur::.ctor()
+extern void klon_olustur__ctor_m3E55A975930E0441E2515E18D7907C4F68A7E837 (void);
+// 0x00000008 System.Void oyuncu::Awake()
+extern void oyuncu_Awake_m60E15079441BFFB9056ACAEFA1B31AC471999047 (void);
+// 0x00000009 System.Void oyuncu::Start()
+extern void oyuncu_Start_m23534D4E8B2E96952215EE1433897D02D60AE274 (void);
+// 0x0000000A System.Void oyuncu::OnCollisionEnter(UnityEngine.Collision)
+extern void oyuncu_OnCollisionEnter_mE23D6D6003EBB9A1829096F3533713E5BA28F089 (void);
+// 0x0000000B System.Void oyuncu::OnTriggerEnter(UnityEngine.Collider)
+extern void oyuncu_OnTriggerEnter_m9DC6A569F5EED0C4A586E499F36341DCE2C3DA62 (void);
+// 0x0000000C System.Void oyuncu::Update()
+extern void oyuncu_Update_m97D3CF68221C9206EA2D91BCE94EF94AAB5B0605 (void);
+// 0x0000000D System.Void oyuncu::hareket()
+extern void oyuncu_hareket_m5F4093697EE1811A1B963D48474FD0DA6DC3C5C5 (void);
+// 0x0000000E System.Void oyuncu::sola_don_basiliyor()
+extern void oyuncu_sola_don_basiliyor_m257400ADDDA210F6ED6076EEB999441A1AACBB91 (void);
+// 0x0000000F System.Void oyuncu::sola_don_cekildi()
+extern void oyuncu_sola_don_cekildi_m24F0F0524C367AA5511EB7D0FC02478561816D7F (void);
+// 0x00000010 System.Void oyuncu::saga_don_basiliyor()
+extern void oyuncu_saga_don_basiliyor_m59348B58B5EE6BC1FCBC192DF73E9A841FFE1EA6 (void);
+// 0x00000011 System.Void oyuncu::saga_don_cekildi()
+extern void oyuncu_saga_don_cekildi_mA506FC6829AC459F533DFC1544CFBCAFA9A4991D (void);
+// 0x00000012 System.Void oyuncu::oyundanCik()
+extern void oyuncu_oyundanCik_m8CA031B55E9A72BBDE4687BA4E782416D87D8038 (void);
+// 0x00000013 System.Void oyuncu::sesAcKapa()
+extern void oyuncu_sesAcKapa_mF3FC47F38BFEDADFB415868E80BFAA71D52DD8CE (void);
+// 0x00000014 System.Void oyuncu::yenidenBaslat()
+extern void oyuncu_yenidenBaslat_m86F16305C4F3E88CBD8D273198121311E4E0FA66 (void);
+// 0x00000015 System.Void oyuncu::durdurBaslat()
+extern void oyuncu_durdurBaslat_mC1D8F53E7412E25D5E3324606B660AB1CA8B6A79 (void);
+// 0x00000016 System.Void oyuncu::.ctor()
+extern void oyuncu__ctor_mCE50A4694CE5D8C73AA25373A6AEA447311C7022 (void);
+// 0x00000017 System.Void ObjectBounce::Start()
+extern void ObjectBounce_Start_m705D85C3F2CDDF0E870CCC4C5181A958AF2A436E (void);
+// 0x00000018 System.Void ObjectBounce::Update()
+extern void ObjectBounce_Update_m5E9660CCC2597D101F625BC2FB36A523A48F1F88 (void);
+// 0x00000019 System.Void ObjectBounce::.ctor()
+extern void ObjectBounce__ctor_m46878B7ACB8B947B982546C8D54BD4A5554F262B (void);
+static Il2CppMethodPointer s_methodPointers[25] = 
+{
+	kamera_LateUpdate_mEE66B361D213B0CA84BE55F362AB38F2EE08C39D,
+	kamera__ctor_mFC815A26481319405330DDC655F061972E58F886,
+	klon_olustur_Start_mA712B5520D4DF322D82A1FE21BDB1655CDD12FDE,
+	klon_olustur_nesne_klonla_mE07B4676DC5D97F671D608776E64FC29EED092E9,
+	klon_olustur_klonla_m6D375F32F51221BB769C63B01F1E0869AD921434,
+	klon_olustur_Update_m50872A12CD8230377F9466675BC7E946AA0C4E57,
+	klon_olustur__ctor_m3E55A975930E0441E2515E18D7907C4F68A7E837,
+	oyuncu_Awake_m60E15079441BFFB9056ACAEFA1B31AC471999047,
+	oyuncu_Start_m23534D4E8B2E96952215EE1433897D02D60AE274,
+	oyuncu_OnCollisionEnter_mE23D6D6003EBB9A1829096F3533713E5BA28F089,
+	oyuncu_OnTriggerEnter_m9DC6A569F5EED0C4A586E499F36341DCE2C3DA62,
+	oyuncu_Update_m97D3CF68221C9206EA2D91BCE94EF94AAB5B0605,
+	oyuncu_hareket_m5F4093697EE1811A1B963D48474FD0DA6DC3C5C5,
+	oyuncu_sola_don_basiliyor_m257400ADDDA210F6ED6076EEB999441A1AACBB91,
+	oyuncu_sola_don_cekildi_m24F0F0524C367AA5511EB7D0FC02478561816D7F,
+	oyuncu_saga_don_basiliyor_m59348B58B5EE6BC1FCBC192DF73E9A841FFE1EA6,
+	oyuncu_saga_don_cekildi_mA506FC6829AC459F533DFC1544CFBCAFA9A4991D,
+	oyuncu_oyundanCik_m8CA031B55E9A72BBDE4687BA4E782416D87D8038,
+	oyuncu_sesAcKapa_mF3FC47F38BFEDADFB415868E80BFAA71D52DD8CE,
+	oyuncu_yenidenBaslat_m86F16305C4F3E88CBD8D273198121311E4E0FA66,
+	oyuncu_durdurBaslat_mC1D8F53E7412E25D5E3324606B660AB1CA8B6A79,
+	oyuncu__ctor_mCE50A4694CE5D8C73AA25373A6AEA447311C7022,
+	ObjectBounce_Start_m705D85C3F2CDDF0E870CCC4C5181A958AF2A436E,
+	ObjectBounce_Update_m5E9660CCC2597D101F625BC2FB36A523A48F1F88,
+	ObjectBounce__ctor_m46878B7ACB8B947B982546C8D54BD4A5554F262B,
+};
+static const int32_t s_InvokerIndices[25] = 
+{
+	3210,
+	3210,
+	3210,
+	3210,
+	1483,
+	3210,
+	3210,
+	3210,
+	3210,
+	2626,
+	2626,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+	3210,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	25,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
